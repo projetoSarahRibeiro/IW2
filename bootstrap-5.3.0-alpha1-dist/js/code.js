a9d6765f7629a1f8e07da3a6207584b758488e8a
Sarah Cristina Ribeiro <100208442+projetoSarahRibeiro@users.noreply.github.com>
2023-03-11 23:56:02 -0300

    Add files via upload

---
+++ bootstrap-5.3.0-alpha1-dist/js/code.js
@@ -0,0 +1,16 @@
+
+function sobre(){
+    window.location.href = "index.html"
+}
+function termo(){
+    window.location.href = "termo.html"
+}
+function exemplos(){
+    window.location.href = "exemplos.html"
+}
+function empresas(){
+    window.location.href = "empresas.html"
+}
+function login(){
+    window.location.href = "login.html"
+}
